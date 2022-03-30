fee7aebe959a35bd214892ca3461a9c4032cd3c7
MikeG01 <mikegoh425@gmail.com>
2022-03-30 23:00:03 +0800

    Attackers Final Version

---
+++ Assets/Scripts/Message2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+[Serializable]
+public class Message2
+{
+    public string sender;
+    public string text;
+    
+    public Message2(string sender, string text)
+    {
+        this.sender = sender;
+        this.text = text;
+    }
+}
